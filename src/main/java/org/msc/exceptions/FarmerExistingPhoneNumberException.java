51d8116cca3db2fb757352f36140d55081efe042
monica <mosice@gmail.com>
2024-12-17 13:15:21 +0100

    feat: add method createFarmer in FarmerService and create exception FarmerExistingPhoneNumberException

---
+++ src/main/java/org/msc/exceptions/FarmerExistingPhoneNumberException.java
@@ -0,0 +1,6 @@
+package org.msc.exceptions;
+
+public class FarmerExistingPhoneNumberException extends RuntimeException {
+    public FarmerExistingPhoneNumberException(String message) { super(message);
+    }
+}
--- src/main/java/org/msc/services/FarmerService.java
+++ src/main/java/org/msc/services/FarmerService.java
@@ -3,6 +3,8 @@ package org.msc.services;
 import org.msc.dtos.FarmerRequest;
 import org.msc.dtos.FarmerResponse;
 import org.msc.entities.Farmer;
+import org.msc.exceptions.FarmerExistingPhoneNumberException;
+import org.msc.mappers.FarmerMapper;
 import org.msc.repositories.FarmerRepository;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +20,15 @@ public class FarmerService {
         this.farmerRepository = farmerRepository;
     }
 
+    public FarmerResponse createFarmer(FarmerRequest farmerRequest){
+        Optional<Farmer> existFarmer = farmerRepository.findByPhone(farmerRequest.phone());
+        if (existFarmer.isPresent())
+            throw new FarmerExistingPhoneNumberException("Farmer already exist with this phone.");
 
+        Farmer farmer = FarmerMapper.toEntity(farmerRequest);
+        Farmer savedFarmer = farmerRepository.save(farmer);
+        return FarmerMapper.toResponse(savedFarmer);
+    }
 
 
 }
