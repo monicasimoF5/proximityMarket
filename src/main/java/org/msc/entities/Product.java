e3b88e22b90e09d811fff6d72bcf1c3443836b3e
monica <mosice@gmail.com>
2024-12-18 11:08:01 +0100

    feat: create new class Product

---
+++ src/main/java/org/msc/entities/Product.java
@@ -0,0 +1,81 @@
+package org.msc.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static java.time.ZoneId.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    private String name;
+    private String type = "unknown";
+
+    public enum Season {
+        WINTER, SPRING, SUMMER, AUTUMN
+    }
+
+    @Enumerated(EnumType.STRING)
+    private Season season;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "farmerId", nullable = false)
+    private Farmer farmer;
+
+    public Product(String name, String type, Season season, Date createdAt, Farmer farmer) {
+        this.name = name;
+        this.type = (type == null || type.isEmpty()) ? "unknown" : type;
+        this.season = season;
+        this.createdAt = createdAt;
+        this.farmer = farmer;
+    }
+
+    @PrePersist
+    public void onCreate(){
+        LocalDateTime now = LocalDateTime.now(of("Europe/Valencia"));
+        Instant instant = now.atZone(ZoneId.systemDefault()).toInstant();
+        createdAt = Date.from(instant);
+
+        Month month = now.getMonth();
+        int dayOfMonth = now.getDayOfMonth();
+        season = getSeason(month, dayOfMonth);
+    }
+
+    private Season getSeason(Month month, int dayOfMonth) {
+        switch (month) {
+            case DECEMBER, JANUARY, FEBRUARY:
+                return season.WINTER;
+            case MARCH:
+                return dayOfMonth >= 20 ? season.SPRING : season.WINTER;
+            case APRIL, MAY:
+                return season.SPRING;
+            case JUNE:
+                return dayOfMonth < 21 ? season.SPRING : season.SUMMER;
+            case JULY, AUGUST:
+                return season.SUMMER;
+            case SEPTEMBER:
+                return dayOfMonth < 23 ? season.SUMMER : season.AUTUMN;
+            case OCTOBER, NOVEMBER:
+                return season.AUTUMN;
+            default:
+                return season.WINTER;
+        }
+    }
+}
